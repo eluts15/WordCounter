f415c44cddaaf246d86ebcc51589f6cb7a9c7fd6
eluts <eluts15@gmai.com>
2017-06-02 09:52:27 -0700

    Test02 fails as expected because the strings are not identical.

--- Tests/WordCounterTests.cs
+++ Tests/WordCounterTests.cs
@@ -9,10 +9,22 @@ namespace WordCounter
     {
       //Arrange
       WordCounter nextWord = new WordCounter("hello world");
-      //ACT
+      //Act
       string result = nextWord.GetUserInput();
       //Assert
       Assert.Equal("hello world", result); //Should now return positive as the strings are equal.
+    }
+
+    [Fact]
+    public void Test02_GetUserInput_ReturnsTrue()
+    {
+      //Arrange
+      WordCounter nextWord = new WordCounter("my, my, my, welcome, welcome, welcome, to the land of Expectations.");
+      //Act
+      string result = nextWord.GetUserInput();
+      //Assert
+      Assert.Equal("My, My, My, welcome, welcome, welcome, to the land of Expectation", result); //Should fail at first, as the string that is passed through the method is
+                                                                                         //not identical to the string that we are Asserting it to.
     }
   }
 }
