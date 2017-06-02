5a17219a2ff3d432dce309581cbd088fce215b6a
eluts <eluts15@gmai.com>
2017-06-02 09:54:01 -0700

    Test02 passes now as expected.

--- Tests/WordCounterTests.cs
+++ Tests/WordCounterTests.cs
@@ -19,11 +19,11 @@ namespace WordCounter
     public void Test02_GetUserInput_ReturnsTrue()
     {
       //Arrange
-      WordCounter nextWord = new WordCounter("my, my, my, welcome, welcome, welcome, to the land of Expectations.");
+      WordCounter nextWord = new WordCounter("My, My, My, welcome, welcome, welcome, to the land of Expectations.");
       //Act
       string result = nextWord.GetUserInput();
       //Assert
-      Assert.Equal("My, My, My, welcome, welcome, welcome, to the land of Expectation", result); //Should fail at first, as the string that is passed through the method is
+      Assert.Equal("My, My, My, welcome, welcome, welcome, to the land of Expectations.", result); //Should fail at first, as the string that is passed through the method is
                                                                                          //not identical to the string that we are Asserting it to.
     }
   }
